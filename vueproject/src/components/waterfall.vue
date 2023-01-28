cacc1ddccd5b25c4bd9bff5a92da2302e657eb7f
LSD <1216395765@qq.com>
2023-01-29 01:45:17 +0800

    vue页面主题布局完善

---
+++ vueproject/src/components/waterfall.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="wrapper">
+    <div class="" v-for="item,index in pics" :key="index">
+      <div class="wf-item">
+        <img class="wf-img" :src="item" alt="" srcset="">
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data(){
+    return {
+      pics:[
+        "https://z3.ax1x.com/2021/09/11/hzMhJ1.jpg",
+        "https://z3.ax1x.com/2021/09/11/hz3Dk4.jpg",
+        "https://z3.ax1x.com/2021/09/11/hz84K0.jpg",
+        "https://z3.ax1x.com/2021/09/11/hz3w0U.jpg",
+        "https://z3.ax1x.com/2021/09/11/hz8R8s.jpg",
+        "https://z3.ax1x.com/2021/09/11/hzMhJ1.jpg",
+        "https://z3.ax1x.com/2021/09/11/hz3Dk4.jpg",
+        "https://z3.ax1x.com/2021/09/11/hz84K0.jpg",
+        "https://z3.ax1x.com/2021/09/11/hz3w0U.jpg",
+        "https://z3.ax1x.com/2021/09/11/hz8R8s.jpg",
+        "https://z3.ax1x.com/2021/09/11/hz8R8s.jpg",
+        "https://z3.ax1x.com/2021/09/11/hzMhJ1.jpg",
+        "https://z3.ax1x.com/2021/09/11/hz3w0U.jpg",
+        "https://z3.ax1x.com/2021/09/11/hz8R8s.jpg",
+        "https://z3.ax1x.com/2021/09/11/hzMhJ1.jpg",
+        "https://z3.ax1x.com/2021/09/11/hz3Dk4.jpg",
+        "https://z3.ax1x.com/2021/09/11/hz84K0.jpg",
+        "https://z3.ax1x.com/2021/09/11/hz3w0U.jpg",
+        "https://z3.ax1x.com/2021/09/11/hz8R8s.jpg",
+        "https://z3.ax1x.com/2021/09/11/hz8R8s.jpg",
+        "https://z3.ax1x.com/2021/09/11/hzMhJ1.jpg",
+        "https://z3.ax1x.com/2021/09/11/hz3w0U.jpg",
+        "https://z3.ax1x.com/2021/09/11/hz8R8s.jpg",
+        "https://z3.ax1x.com/2021/09/11/hzMhJ1.jpg",
+        "https://z3.ax1x.com/2021/09/11/hz3Dk4.jpg",
+        "https://z3.ax1x.com/2021/09/11/hz84K0.jpg",
+        "https://z3.ax1x.com/2021/09/11/hz3w0U.jpg",
+        "https://z3.ax1x.com/2021/09/11/hz8R8s.jpg",
+        "https://z3.ax1x.com/2021/09/11/hz8R8s.jpg",
+        "https://z3.ax1x.com/2021/09/11/hzMhJ1.jpg",
+        "https://z3.ax1x.com/2021/09/11/hz3w0U.jpg",
+        "https://z3.ax1x.com/2021/09/11/hz8R8s.jpg",
+        "https://z3.ax1x.com/2021/09/11/hzMhJ1.jpg",
+        "https://z3.ax1x.com/2021/09/11/hz3Dk4.jpg",
+        "https://z3.ax1x.com/2021/09/11/hz84K0.jpg",
+        "https://z3.ax1x.com/2021/09/11/hz3w0U.jpg",
+        "https://z3.ax1x.com/2021/09/11/hz8R8s.jpg",
+        "https://z3.ax1x.com/2021/09/11/hz8R8s.jpg",
+        "https://z3.ax1x.com/2021/09/11/hzMhJ1.jpg",
+        "https://z3.ax1x.com/2021/09/11/hz3w0U.jpg",
+        "https://z3.ax1x.com/2021/09/11/hz8R8s.jpg",
+        "https://z3.ax1x.com/2021/09/11/hzMhJ1.jpg",
+        "https://z3.ax1x.com/2021/09/11/hz3Dk4.jpg",
+        "https://z3.ax1x.com/2021/09/11/hz84K0.jpg",
+        "https://z3.ax1x.com/2021/09/11/hz3w0U.jpg",
+        "https://z3.ax1x.com/2021/09/11/hz8R8s.jpg",
+        "https://z3.ax1x.com/2021/09/11/hz8R8s.jpg",
+        "https://z3.ax1x.com/2021/09/11/hzMhJ1.jpg",
+        "https://z3.ax1x.com/2021/09/11/hz3w0U.jpg",
+        "https://z3.ax1x.com/2021/09/11/hz8R8s.jpg",
+        "https://z3.ax1x.com/2021/09/11/hzMhJ1.jpg",
+        "https://z3.ax1x.com/2021/09/11/hz3Dk4.jpg",
+        "https://z3.ax1x.com/2021/09/11/hz84K0.jpg",
+        "https://z3.ax1x.com/2021/09/11/hz3w0U.jpg",
+        "https://z3.ax1x.com/2021/09/11/hz8R8s.jpg",
+        "https://z3.ax1x.com/2021/09/11/hz8R8s.jpg",
+        "https://z3.ax1x.com/2021/09/11/hzMhJ1.jpg",
+      ]
+    }
+  },
+  setup() {
+    
+  },
+}
+</script>
+<style>
+.wrapper{
+    position: relative;
+    width: 1200px;
+    margin: 0px auto;
+    columns: 6;
+    columns-gap: 40px;
+    border: 1px solid #66ccff;
+} 
+.wf-item{
+  margin: 10px auto;
+}
+
+.wf-item .wf-img{
+    width: 100%;
+    height: 100%;
+}
+
+</style>
--- vueproject/src/views/Home.vue
+++ vueproject/src/views/Home.vue
@@ -1,18 +1,173 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+<el-container style="height: 100%; border: 1px solid #eee">
+  <el-container>
+    
+    <el-header style=" font-size: 12px; border-bottom:1px solid #ccc; ">
+      <el-row>
+        <el-col :span="6">
+          <div id="nav">
+            <router-link class="homelink" to="/">Home</router-link> |
+            <router-link class="homelink" to="/about">About</router-link>
+            <router-link class="homelink" to="/about">About</router-link>
+            <router-link class="homelink" to="/about">About</router-link>
+          </div>
+        </el-col>
+        <el-col :span="14">      
+            <div >
+              <el-input 
+              placeholder="请输入内容" 
+              v-model="searchinput"
+              class="input-with-select">
+
+                <el-select 
+                  slot="prepend" 
+                  v-model="select"
+                  placeholder="请选择">
+
+                  <el-option label="餐厅名" value="1"></el-option>
+                  <el-option label="订单号" value="2"></el-option>
+                  <el-option label="用户电话" value="3"></el-option>
+                </el-select>
+                <el-button slot="append" icon="el-icon-search"></el-button>
+              </el-input>
+            </div>
+        </el-col>
+        <el-col :span="4" style="text-align: right;"> 
+            <el-dropdown>
+                  <i class="el-icon-setting" style="margin-right: 15px"></i>
+                  <el-dropdown-menu slot="dropdown">
+                    <el-dropdown-item>查看</el-dropdown-item>
+                    <el-dropdown-item>新增</el-dropdown-item>
+                    <el-dropdown-item>删除</el-dropdown-item>
+                  </el-dropdown-menu>
+            </el-dropdown>
+            <span>王小虎</span>
+        </el-col>
+      </el-row>
+      
+
+
+    </el-header>
+    
+    <el-main>
+        <el-row style="margin: 15px">
+          <el-col :span="6">
+            <div>
+              <el-radio-group v-model="homeradio1">
+                <el-radio-button label="上海"></el-radio-button>
+                <el-radio-button label="北京"></el-radio-button>
+                <el-radio-button label="广州"></el-radio-button>
+                <el-radio-button label="深圳"></el-radio-button>
+              </el-radio-group>
+            </div>
+          </el-col>
+          <el-col :span="18"  style="text-align: right;">
+            <el-dropdown>
+              <el-button type="primary">
+                更多菜单<i class="el-icon-arrow-down el-icon--right"></i>
+              </el-button>
+              <el-dropdown-menu slot="dropdown">
+                <el-dropdown-item>黄金糕</el-dropdown-item>
+                <el-dropdown-item>狮子头</el-dropdown-item>
+                <el-dropdown-item>螺蛳粉</el-dropdown-item>
+                <el-dropdown-item>双皮奶</el-dropdown-item>
+                <el-dropdown-item>蚵仔煎</el-dropdown-item>
+              </el-dropdown-menu>
+            </el-dropdown>
+            <el-dropdown>
+              <el-button type="primary">
+                更多菜单<i class="el-icon-arrow-down el-icon--right"></i>
+              </el-button>
+              <el-dropdown-menu slot="dropdown">
+                <el-dropdown-item>黄金糕</el-dropdown-item>
+                <el-dropdown-item>狮子头</el-dropdown-item>
+                <el-dropdown-item>螺蛳粉</el-dropdown-item>
+                <el-dropdown-item>双皮奶</el-dropdown-item>
+                <el-dropdown-item>蚵仔煎</el-dropdown-item>
+              </el-dropdown-menu>
+            </el-dropdown>
+            <el-dropdown>
+              <el-button type="primary">
+                更多菜单<i class="el-icon-arrow-down el-icon--right"></i>
+              </el-button>
+              <el-dropdown-menu slot="dropdown">
+                <el-dropdown-item>黄金糕</el-dropdown-item>
+                <el-dropdown-item>狮子头</el-dropdown-item>
+                <el-dropdown-item>螺蛳粉</el-dropdown-item>
+                <el-dropdown-item>双皮奶</el-dropdown-item>
+                <el-dropdown-item>蚵仔煎</el-dropdown-item>
+              </el-dropdown-menu>
+            </el-dropdown>
+
+
+          </el-col>
+        </el-row>
+      <Waterfall></Waterfall>
+
+      <el-footer >
+        <div style="text-align: center;">
+          <h1>查看更多内容请登录</h1>
+        </div>
+      </el-footer>
+
+    </el-main>
+
+
+  </el-container>
+
+
+</el-container>
+
+
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Waterfall from '@/components/waterfall.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
-  }
+    Waterfall
+  },
+  data() {
+    return{
+      searchinput:"",
+      select:"",
+      homeradio1:"上海",
+    }
+    }
 }
 </script>
+<style>
+  .el-header {
+    background-color: #B3C0D1;
+    color: #333;
+    line-height: 60px;
+  }
+  
+  .el-aside {
+    color: #333;
+  }
+  .homelink{
+    margin-left:8px;
+    margin-right:8px;
+  }
+
+  .el-dropdown {
+    vertical-align: top;
+  }
+  .el-dropdown + .el-dropdown {
+    margin-left: 15px;
+  }
+  .el-icon-arrow-down {
+    font-size: 12px;
+  }
+
+
+</style>
+
